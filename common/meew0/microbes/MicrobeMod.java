48bb844c79ef3770ba8a19c89f6c6f1304930214
meew0 <blactbt@live.de>
2013-06-28 16:38:07 +0200

    First commit! :D

---
+++ common/meew0/microbes/MicrobeMod.java
@@ -0,0 +1,8 @@
+package meew0.microbes;
+
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid = MicrobeMod.modid, name="Microbes", version = "0.1a")
+public class MicrobeMod {
+    public static final String modid = "meew0_microbes";
+}
