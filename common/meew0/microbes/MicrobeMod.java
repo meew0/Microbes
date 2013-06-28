aa2c3755a071e606d32df25047162575a2703e80
meew0 <blactbt@live.de>
2013-06-28 18:30:41 +0200

    Some more commenting

--- common/meew0/microbes/MicrobeMod.java
+++ common/meew0/microbes/MicrobeMod.java
@@ -2,8 +2,10 @@ package meew0.microbes;
 
 import java.util.logging.Logger;
 
+import meew0.microbes.proxy.CommonProxy;
 import cpw.mods.fml.common.FMLLog;
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.Mod.*;
 import cpw.mods.fml.common.event.*;
 import cpw.mods.fml.common.network.NetworkMod;
@@ -11,24 +13,36 @@ import cpw.mods.fml.common.network.NetworkMod;
 @Mod(modid = MicrobeMod.id, name = MicrobeMod.modName, version = MicrobeMod.modVersion)
 @NetworkMod(clientSideRequired = true, serverSideRequired = false)
 public class MicrobeMod {
+    // Random variables
     public static final String id = "Microbes";
     public static final String modName = "Microbes";
     public static final String modVersion = "0.1a";
     
-    @Instance(id)
-    public static MicrobeMod mod = new MicrobeMod();
+    // Items
     
-    private Logger lg = Logger.getLogger(id);
+    // Blocks
+    
+    @SidedProxy(serverSide = "meew0.microbes.proxy.CommonProxy", clientSide = "meew0.microbes.proxy.ClientProxy")
+    public static CommonProxy serverProxy = new CommonProxy(); // Create proxy
+    
+    @Instance(id)
+    public static MicrobeMod mod = new MicrobeMod(); // Create mod instance
+    
+    private Logger lg = Logger.getLogger(id); // Create logger
     
     @PreInit
     public void preInit(FMLPreInitializationEvent evt) {
-        lg.setParent(FMLLog.getLogger());
+        lg.setParent(FMLLog.getLogger()); // Initialize logger
         lg.info(getFullModName() + " loaded, initializing now");
     }
     
     @Init
     public void init(FMLInitializationEvent evt) {
+        // Initialize items
         
+        
+        
+        // Initialize blocks
     }
     
     @PostInit
