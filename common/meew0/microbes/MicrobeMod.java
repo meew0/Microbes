6f30b1c842f94f08edb2e0288041a1255921cb8d
meew0 <blactbt@live.de>
2013-06-28 17:55:23 +0200

    Random stuffs

--- common/meew0/microbes/MicrobeMod.java
+++ common/meew0/microbes/MicrobeMod.java
@@ -1,8 +1,46 @@
 package meew0.microbes;
 
-import cpw.mods.fml.common.Mod;
+import java.util.logging.Logger;
 
-@Mod(modid = MicrobeMod.modid, name="Microbes", version = "0.1a")
+import cpw.mods.fml.common.FMLLog;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.*;
+import cpw.mods.fml.common.event.*;
+import cpw.mods.fml.common.network.NetworkMod;
+
+@Mod(modid = MicrobeMod.id, name = MicrobeMod.modName, version = MicrobeMod.modVersion)
+@NetworkMod(clientSideRequired = true, serverSideRequired = false)
 public class MicrobeMod {
-    public static final String modid = "meew0_microbes";
+    public static final String id = "Microbes";
+    public static final String modName = "Microbes";
+    public static final String modVersion = "0.1a";
+    
+    @Instance(id)
+    public static MicrobeMod mod = new MicrobeMod();
+    
+    private Logger lg = Logger.getLogger(id);
+    
+    @PreInit
+    public void preInit(FMLPreInitializationEvent evt) {
+        lg.setParent(FMLLog.getLogger());
+        lg.info("Microbes loaded, initializing now");
+    }
+    
+    @Init
+    public void init(FMLInitializationEvent evt) {
+        
+    }
+    
+    @PostInit
+    public void postInit(FMLPostInitializationEvent evt) {
+        lg.info("Microbes successfully initialized");
+    }
+    
+    public static String getFullModName() {
+        return modName + " " + modVersion;
+    }
+    
+    public Logger logger() {
+        return lg;
+    }
 }
